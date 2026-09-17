25c8674aa7a5b7acb78328245bedd6b84ed1b5f4
eldudyaguirre <143123379+eldudyaguirre@users.noreply.github.com>
2026-09-17 11:58:30 -0500

    Add Django route for patient profile

--- store/urls.py
+++ store/urls.py
@@ -39,6 +39,7 @@ urlpatterns = [
 
     path('homein-calendario/', views.homeincalendario, name='homeincalendario'),
     path('homein-pacientes/', views.homeinpacientes, name='homeinpacientes'),
+    path('homein-pacientes/perfil/', views.homeinperfilpaciente, name='homeinperfilpaciente'),
     path('homein-nuevopaciente/', views.homeinnuevopaciente, name='homeinnuevopaciente'),
     path('homein-nuevacita/', views.homeinnuevacita, name='homeinnuevacita'),
     path('sitemap.xml', sitemap, {'sitemaps': sitemaps}, name='django.contrib.sitemaps.views.sitemap'),
