d5d36f76ea0915adf4b3cf513cd73389bc4214b2
eldudyaguirre <143123379+eldudyaguirre@users.noreply.github.com>
2026-09-26 09:03:17 -0500

    Rutas privadas y URLs dinamicas del blog

--- store/urls.py
+++ store/urls.py
@@ -1,47 +1,38 @@
-from django.urls import path, include
-from . import views
-from django.contrib.auth.views import LogoutView
-from django.contrib.auth import views as auth_views
 from django.contrib.sitemaps.views import sitemap
+from django.urls import path
+from . import views
 from store.sitemaps import StaticViewSitemap
 
-sitemaps = {
-    'static': StaticViewSitemap,
-}
+sitemaps = {"static": StaticViewSitemap}
 
 urlpatterns = [
-    path('', views.home, name='home'),
-    path('inicio/', views.homein, name='homein'),
-
-    path('about/', views.about, name='about'),
-    path('contact/', views.contactanos, name='contact'),
-    path('agendamiento/', views.agendamiento, name='agendamiento'),
-
-    path('services/', views.services, name='services'),
-    path('individualtherapy/', views.individualtherapy, name='individualtherapy'),
-    path('coupletherapy/', views.coupletherapy, name='coupletherapy'),
-    path('grupaltherapy/', views.grupaltherapy, name='grupaltherapy'),
-    path('kidstherapy/', views.kidstherapy, name='kidstherapy'),
-    path('depansitherapy/', views.depansitherapy, name='depansitherapy'),
-    path('judgeservices/', views.judgeservices, name='judgeservices'),
-    path('meditation/', views.meditation, name='meditation'),
-    path('blog-general/', views.bloggeneral, name='bloggeneral'),
-    path('blog-detalle/', views.blogdetalle, name='blogdetalle'),
-
-    path('ayuda-sos/', views.ayudasos, name='ayudasos'),
-    path('guerrero-valiente/', views.guerrerovaliente, name='guerrerovaliente'),
-    path('kchetazo-mental/', views.kchetazomental, name='kchetazomental'),
-    path('team-hp/', views.teamhp, name='teamhp'),
-
-    path('signin/', views.signin, name='signin'),
-    path('logout/', views.do_logout, name='logout'),
-    path('homein/', views.homein, name='homein'),
-
-    path('homein-calendario/', views.homeincalendario, name='homeincalendario'),
-    path('homein-pacientes/', views.homeinpacientes, name='homeinpacientes'),
-    path('homein-pacientes/perfil/', views.homeinperfilpaciente, name='homeinperfilpaciente'),
-    path('homein-pacientes/editar/', views.homeineditarpaciente, name='homeineditarpaciente'),
-    path('homein-nuevopaciente/', views.homeinnuevopaciente, name='homeinnuevopaciente'),
-    path('homein-nuevacita/', views.homeinnuevacita, name='homeinnuevacita'),
-    path('sitemap.xml', sitemap, {'sitemaps': sitemaps}, name='django.contrib.sitemaps.views.sitemap'),
+    path("", views.home, name="home"),
+    path("inicio/", views.homein, name="homein"),
+    path("about/", views.about, name="about"),
+    path("contact/", views.contactanos, name="contact"),
+    path("agendamiento/", views.agendamiento, name="agendamiento"),
+    path("services/", views.services, name="services"),
+    path("individualtherapy/", views.individualtherapy, name="individualtherapy"),
+    path("coupletherapy/", views.coupletherapy, name="coupletherapy"),
+    path("grupaltherapy/", views.grupaltherapy, name="grupaltherapy"),
+    path("kidstherapy/", views.kidstherapy, name="kidstherapy"),
+    path("depansitherapy/", views.depansitherapy, name="depansitherapy"),
+    path("judgeservices/", views.judgeservices, name="judgeservices"),
+    path("meditation/", views.meditation, name="meditation"),
+    path("blog-general/", views.bloggeneral, name="bloggeneral"),
+    path("blog/<slug:slug>/", views.blogdetalle, name="blogdetalle"),
+    path("ayuda-sos/", views.ayudasos, name="ayudasos"),
+    path("guerrero-valiente/", views.guerrerovaliente, name="guerrerovaliente"),
+    path("kchetazo-mental/", views.kchetazomental, name="kchetazomental"),
+    path("team-hp/", views.teamhp, name="teamhp"),
+    path("signin/", views.signin, name="signin"),
+    path("logout/", views.do_logout, name="logout"),
+    path("homein/", views.homein, name="homein"),
+    path("homein-calendario/", views.homeincalendario, name="homeincalendario"),
+    path("homein-pacientes/", views.homeinpacientes, name="homeinpacientes"),
+    path("homein-pacientes/perfil/", views.homeinperfilpaciente, name="homeinperfilpaciente"),
+    path("homein-pacientes/editar/", views.homeineditarpaciente, name="homeineditarpaciente"),
+    path("homein-nuevopaciente/", views.homeinnuevopaciente, name="homeinnuevopaciente"),
+    path("homein-nuevacita/", views.homeinnuevacita, name="homeinnuevacita"),
+    path("sitemap.xml", sitemap, {"sitemaps": sitemaps}, name="django.contrib.sitemaps.views.sitemap"),
 ]
