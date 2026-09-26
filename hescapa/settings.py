4c07cadd77f1c3b2a2bad03eea2e141301a35c30
eldudyaguirre <143123379+eldudyaguirre@users.noreply.github.com>
2026-09-25 19:36:10 -0500

    Configurar carpeta Media para archivos subidos

--- hescapa/settings.py
+++ hescapa/settings.py
@@ -196,7 +196,7 @@ STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 MEDIA_URL = '/media/'
-MEDIA_ROOT = BASE_DIR / 'media'
+MEDIA_ROOT = BASE_DIR / 'Media'
 
 # Login Config
 
