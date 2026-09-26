b7ab9bac2ef7ff4aeb0cfed3ccae85398029708e
eldudyaguirre <143123379+eldudyaguirre@users.noreply.github.com>
2026-09-26 09:14:14 -0500

    Limpiar importación del control de acceso

--- store/views.py
+++ store/views.py
@@ -1,6 +1,5 @@
 from django.contrib import messages
 from django.contrib.auth import login, logout
-from django.contrib.auth.decorators import user_passes_test
 from django.contrib.auth.forms import AuthenticationForm
 from django.shortcuts import get_object_or_404, redirect, render
 from django.urls import reverse
